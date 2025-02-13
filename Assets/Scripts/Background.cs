f3e07539439a63acc9549818b3b363d2aa93a044
YoungMoo Kim <rladuan612@gmail.com>
2025-02-13 21:38:48 +0900

    create character

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{   
+    private float moveSpeed = 3f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.down *moveSpeed *Time.deltaTime;
+        if (transform.position.y < -12) {
+            transform.position += new Vector3(0,24f,0);
+        }
+    }
+}
